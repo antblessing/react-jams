626b2f3a787221e0980d69480b24bad16a21f989
antblessing <antblessing@gmail.com>
2018-03-27 00:26:50 -0700

    Added landing and library content and dummy album data

---
+++ src/components/Album.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return(
+      <section className="album">
+        {this.props.match.params.slug} Album
+      </section>
+    );
+  }
+}
+
+export default Album;
